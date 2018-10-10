6b1f5229877283300209af8a9aa773a50c5b3d9c
TaikiKoyama <taikikoyama0701@gmail.com>
2018-10-11 09:18:08 +1000

    Add event page file

---
+++ iCulture/src/components/EventPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="event-page frame">
+        event
+    </div>
+</template>
+
+<script>
+export default {
+    name: "EventPage"
+}
+</script>
+
+<style lang="scss" scoped>
+.event-page {
+
+}
+</style>
+
--- iCulture/src/router/index.js
+++ iCulture/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import EventList from '@/components/EventList'
+import EventPage from '@/components/EventPage'
+
 
 
 Vue.use(Router)
@@ -17,6 +19,11 @@ export default new Router({
       path: '/EventList',
       name: 'EventList',
       component: EventList
+    },
+    {
+      path: '/EventPage',
+      name: 'EventPage',
+      component: EventPage
     }
   ]
 })
