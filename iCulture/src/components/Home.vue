c7e4f8be04a8f3cef282fbe3c486dfc27fcc6803
TaikiKoyama <taikikoyama0701@gmail.com>
2018-10-11 08:05:08 +1000

    add scss loader

---
+++ iCulture/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        home page
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+    
+}
+</style>
+
+
